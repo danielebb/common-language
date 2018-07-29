d81f3b3bfff3a980c9f1df0d60953d805c79c645
danielebb <d.busin@modalsource.com>
2018-07-29 16:20:05 +0200

    get Language reference from subclass

--- common-language-loader-static/src/main/java/it/dbb/common/language/loader/statics/BaseStaticCommonLanguageLoader.java
+++ common-language-loader-static/src/main/java/it/dbb/common/language/loader/statics/BaseStaticCommonLanguageLoader.java
@@ -7,6 +7,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.CacheResourceBundleLoader;
 import com.liferay.portal.kernel.util.ClassResourceBundleLoader;
@@ -14,6 +15,11 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ResourceBundleLoader;
 
 import it.dbb.common.language.bundleloader.api.CommonLanguageLoader;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author danielebb
@@ -40,21 +46,26 @@ public class BaseStaticCommonLanguageLoader implements CommonLanguageLoader {
 
 		Set<String> resourceBundleLoaderLanguageIds = new HashSet<>();
 
-		for (Locale locale : LanguageUtil.getAvailableLocales()) {
-
-			try {
-
-				ResourceBundle resourceBundle = ResourceBundle.getBundle(getResourceLanguagePath(), locale,
-						this.getClass().getClassLoader());
-
-				if (resourceBundle != null) {
-
-					resourceBundleLoaderLanguageIds.add(LocaleUtil.toLanguageId(locale));
+		Language language = getInstance();
+		
+		if(language != null) {
+			
+			for (Locale locale : language.getAvailableLocales()) {
+				
+				try {
+					
+					ResourceBundle resourceBundle = ResourceBundle.getBundle(getResourceLanguagePath(), locale,
+							this.getClass().getClassLoader());
+					
+					if (resourceBundle != null) {
+						
+						resourceBundleLoaderLanguageIds.add(LocaleUtil.toLanguageId(locale));
+					}
+					
+				} catch (MissingResourceException mre) {
+					
+					// no bundle for this locale
 				}
-
-			} catch (MissingResourceException mre) {
-
-				// no bundle for this locale
 			}
 		}
 
@@ -63,4 +74,33 @@ public class BaseStaticCommonLanguageLoader implements CommonLanguageLoader {
 
 	private ResourceBundleLoader staticBundleLoader = new CacheResourceBundleLoader(
 			new ClassResourceBundleLoader(getResourceLanguagePath(), this.getClass().getClassLoader()));
+
+	private Language language;
+	
+	protected void setLanguage(Language language) {
+		
+		this.language = language;
+	}
+	
+	private Language getInstance() {
+		
+		if(language == null) {
+			
+			Bundle bundle = FrameworkUtil.getBundle(this.getClass());
+			
+			BundleContext bundleContext = bundle.getBundleContext();
+			
+			if(bundleContext != null) {
+				
+				ServiceReference<Language> serviceReference = bundleContext.getServiceReference(Language.class);
+				
+				if(serviceReference != null) {
+					
+					language = bundleContext.getService(serviceReference);
+				}
+			}
+		}
+		
+		return language;
+	}
 }
--- common-language-loader-static/src/main/java/it/dbb/common/language/loader/statics/sample/StaticCommonLanguageLoader.java
+++ common-language-loader-static/src/main/java/it/dbb/common/language/loader/statics/sample/StaticCommonLanguageLoader.java
@@ -1,9 +1,11 @@
 package it.dbb.common.language.loader.statics.sample;
 
+import com.liferay.portal.kernel.language.Language;
 import org.osgi.service.component.annotations.Component;
 
 import it.dbb.common.language.bundleloader.api.CommonLanguageLoader;
 import it.dbb.common.language.loader.statics.BaseStaticCommonLanguageLoader;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * 
@@ -12,5 +14,11 @@ import it.dbb.common.language.loader.statics.BaseStaticCommonLanguageLoader;
  */
 @Component(property = { "it.dbb.common.language.type=static" }, service = CommonLanguageLoader.class)
 public class StaticCommonLanguageLoader extends BaseStaticCommonLanguageLoader {
-
+    
+    @Override
+    @Reference(unbind = "-", service = Language.class)
+    protected void setLanguage(Language language) {
+        
+        super.setLanguage(language);
+    }
 }
